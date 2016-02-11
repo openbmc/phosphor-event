8890b946d10b5d8b2d0b27857d50e096d6286082
Chris Austen <austenc@us.ibm.com>
2016-02-11 15:37:49 -0600

    Tabs, spaces and columns
    you wanted it you got it.  Indentation set to tabs, shrunk some
    lines to adhere to 80 columns.  All in one commit.

--- event_messaged.C
+++ event_messaged.C
@@ -6,60 +6,65 @@ const char *path_to_messages = "/var/lib/obmc/events";
 
 using namespace std;
 
-void message_refresh_events(event_manager *em) {
-    em->next_log_refresh();
-    return;
+void message_refresh_events(event_manager *em)
+{
+	em->next_log_refresh();
+	return;
 }
-uint16_t message_next_event(event_manager *em) {
-    return em->next_log();
-}
-
-uint16_t message_create_new_log_event(event_manager *em, event_record_t *rec) {
-    return em->create(rec);
-}
-int message_load_log(event_manager *em,uint16_t logid, event_record_t **rec) {
-    return em->open(logid, rec);
-}
-void message_free_log(event_manager *em, event_record_t *rec) {
-    return em->close(rec);
-}
-int message_delete_log(event_manager *em, uint16_t logid) {
-    return em->remove(logid);
+uint16_t message_next_event(event_manager *em)
+{
+	return em->next_log();
 }
 
-int load_existing_events(event_manager *em) {
-
-    uint16_t id;
-
-    while ( (id = em->next_log()) != 0) {
-        send_log_to_dbus(em, id);
-    }
-
-    return 0;
+uint16_t message_create_new_log_event(event_manager *em, event_record_t *rec)
+{
+	return em->create(rec);
+}
+int message_load_log(event_manager *em,uint16_t logid, event_record_t **rec)
+{
+	return em->open(logid, rec);
+}
+void message_free_log(event_manager *em, event_record_t *rec)
+{
+	return em->close(rec);
+}
+int message_delete_log(event_manager *em, uint16_t logid)
+{
+	return em->remove(logid);
 }
 
-int main(int argc, char *argv[]) {
+int load_existing_events(event_manager *em)
+{
+	uint16_t id;
 
-    int rc = 0;
-    event_manager em(path_to_messages);
+	while ( (id = em->next_log()) != 0) {
+		send_log_to_dbus(em, id);
+	}
 
+	return 0;
+}
 
-    rc = build_bus(&em);
-    if (rc < 0) {
-        fprintf(stderr, "Event Messager failed to connect to dbus rc=%d", rc);
-        goto finish;
-    }
+int main(int argc, char *argv[])
+{
+	int rc = 0;
+	event_manager em(path_to_messages);
 
-    rc = load_existing_events(&em);
-    if (rc < 0) {
-        fprintf(stderr, "Event Messager failed add previous logs to dbus rc=%d", rc);
-        goto finish;
-    }
+	rc = build_bus(&em);
+	if (rc < 0) {
+		fprintf(stderr, "Event Messager failed to connect to dbus rc=%d", rc);
+		goto finish;
+	}
 
-    rc = start_event_monitor();
+	rc = load_existing_events(&em);
+	if (rc < 0) {
+		fprintf(stderr, "Event Messager failed add previous logs to dbus rc=%d", rc);
+		goto finish;
+	}
+
+	rc = start_event_monitor();
 
 finish:
-    cleanup_event_monitor();
+	cleanup_event_monitor();
 
-    return rc;
+	return rc;
 }

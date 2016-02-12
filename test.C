7c101c32c93106b7a6d3f96d3b4d9a1c3368497f
Chris Austen <austenc@us.ibm.com>
2016-02-11 23:04:05 -0600

    Convert to known version of assert
    I made my own ASSERT complete with cool text.  I was encouraged
    to use existing C tooling. So here you go

--- test.C
+++ test.C
@@ -3,14 +3,13 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string>
+#include <assert.h>
 
 #include "message.H"
 
 using namespace std;
 
 string eventspath = "./events";
-
-#define ASSERT(a,b,c)  if(b!=c) {cout << "ASSERT ERROR: line " << __LINE__ << endl; exit(1);} else { cout << "Passed: " << a << endl;}
 
 void build_event_record(event_record_t *rec,
 			const char *message,
@@ -30,8 +29,8 @@ void build_event_record(event_record_t *rec,
 	return;
 }
 
-int main(int argc, char *argv[]) {
-
+int main(int argc, char *argv[])
+{
 	uint8_t p[] = {0x3, 0x32, 0x34, 0x36};
 	event_record_t rec, *prec;
 	string s;
@@ -41,69 +40,68 @@ int main(int argc, char *argv[]) {
 
 	event_manager m(eventspath);
 
-	ASSERT("Empty DB", m.get_managed_size(), 0);
+	assert(m.get_managed_size() == 0);
 
-	ASSERT("No Logs", m.next_log(), 0);
-	ASSERT("Double No Logs", m.next_log(), 0);
+	assert(m.next_log() == 3);
+	assert(m.next_log() == 0);
 	m.next_log_refresh();
-	ASSERT("No Logs Refresh", m.next_log(), 0);
-	ASSERT("latest log 0", m.latest_log_id(), 0);
-	ASSERT("Log Entries 0", m.log_count(), 0);
+	assert(m.next_log() == 0);
+	assert(m.latest_log_id() == 0);
+	assert(m.log_count() == 0);
 
 	// Building 1 Event log
 	build_event_record(&rec,"Testing Message1", "Info", "Association", "Test", p, 4);
-	ASSERT("Create log 1", m.create(&rec), 1);
-	ASSERT("1 log is 36 byte", m.get_managed_size(), 75);
-	ASSERT("Log Entries 0", m.log_count(), 1);
-	ASSERT("latest log 1", m.latest_log_id(), 1);
+	assert(m.create(&rec) == 1);
+	assert(m.get_managed_size() == 75);
+	assert(m.log_count() == 1);
+	assert(m.latest_log_id() == 1);
 	m.next_log_refresh();
-	ASSERT("Next log w/ 1", m.next_log(), 1);
+	assert(m.next_log() == 1);
 	m.next_log_refresh();
 
 	// Building 2nd Event log
 	build_event_record(&rec,"Testing Message2", "Info", "Association", "Test", p, 4);
-	ASSERT("Create log 2", m.create(&rec), 2);
-	ASSERT("double event manager size", m.get_managed_size(), 150);
-	ASSERT("Log Entries 0", m.log_count(), 2);
-	ASSERT("latest log 2", m.latest_log_id(), 2);
+	assert(m.create(&rec) == 2);
+	assert(m.get_managed_size() == 150);
+	assert(m.log_count() == 2);
+	assert(m.latest_log_id() == 2);
 	m.next_log_refresh();
-	ASSERT("Next Logs scan 1", m.next_log(), 1);
-	ASSERT("Next Logs scan 2", m.next_log(), 2);
+	assert(m.next_log() == 1);
+	assert(m.next_log() == 2);
 
 	// Read Log 1
-	ASSERT("Log 1 load", m.open(1, &prec), 1);
+	assert(m.open(1, &prec) == 1);
 	s = prec->message;
-	ASSERT("Log 1 validation", s.compare("Testing Message1"), 0);
+	assert(s.compare("Testing Message1") == 0);
 	m.close(prec);
 
 	// Read Log 2
-	ASSERT("Log 2 load", m.open(2, &prec), 2);
+	assert(m.open(2, &prec) == 2);
 	s = prec->message;
-	ASSERT("Log 2 validation", s.compare("Testing Message2"), 0);
+	assert(s.compare("Testing Message2") == 0);
 	m.close(prec);
 
 	// Lets delete the earlier log, then create a new event manager
 	// the latest_log_id should still be 2
 	m.remove(1);
-	ASSERT("manage size with deletion", m.get_managed_size(), 75);
+	assert(m.get_managed_size() == 75);
 
 	event_manager q(eventspath);
-	ASSERT("latest log 2", q.latest_log_id(), 2);
-	ASSERT("Log Entries 0", q.log_count(), 1);
+	assert(q.latest_log_id() == 2);
+	assert(q.log_count() == 1);
 	m.next_log_refresh();
-
 
 	// Travese log list stuff
 	system("exec rm -r ./events/* 2> /dev/null");
 	event_manager a(eventspath);
-	ASSERT("init next log with no logs", a.next_log(), 0);
+	assert(a.next_log() == 0);
 
 	build_event_record(&rec,"Testing list", "Info", "Association", "Test", p, 4);
 	a.create(&rec);
 	a.create(&rec);
 
 	event_manager b(eventspath);
-	ASSERT("init next log with 1 log", b.next_log(), 1);
+	assert(b.next_log() == 1);
 
 	return 0;
 }

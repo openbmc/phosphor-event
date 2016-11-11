456e22d0bfa76dc025316b876a39d42236efe3b9
Josh King <jdking@us.ibm.com>
2016-11-11 13:03:22 -0600

    Broke down our unit tests into gtest functions
    I analyzed test.C to see which of the tests we
    had created and were running.  I took the broken
    down tests and went over to the utest.cpp to start
    the functions and listed them out.  These functions
    still need to be implemented and that is my next
    step.  I will use test.C to complete the implementation.
    This commit was merly only for the breaking down of
    the functions.
    
    Change-Id: Ib3a75b3e2d4a7fba71b4ea337e552a4505100f5f
    Signed-off-by: Josh King <jdking@us.ibm.com>

--- test/utest.cpp
+++ test/utest.cpp
@@ -1,12 +1,22 @@
 #include "sample.h"
 #include "message.hpp"
+#include "event_messaged.hpp"
 #include <gtest/gtest.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <assert.h>
+#include <unistd.h>
 
-TEST(FactorialTest, Zero) {
-    EXPECT_EQ(1, Factorial(0));
-}
 
 string eventspath = "./events";
+uint8_t p[] = {0x3, 0x32, 0x34, 0x36};
+event_record_t rec, *prec;
+string s;
+event_manager m(eventspath, 0, 0);
+
+
 
 void build_event_record(event_record_t *rec,
                         const char *message,
@@ -41,14 +51,113 @@ void setup(void)
         return;
 }
 
+/* First sample Test */
+TEST(FactorialTest, Zero) {
+    EXPECT_EQ(1, Factorial(0));
+}
+
+
+/* Building 1st Event Log */
+TEST(BuildingEventLog, one) {
+   EXPECT_EQ(1, 1);
+   build_event_record(&rec,"Testing Message1", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(1,  m.create(&rec));
+   EXPECT_EQ(75, m.get_managed_size());
+   EXPECT_EQ(1,  m.log_count());
+   EXPECT_EQ(1,  m.latest_log_id());
+   m.next_log_refresh();
+   EXPECT_EQ(1,  m.next_log());
+   m.next_log_refresh();
+}
+
+
+/* Building 2nd Event Log */
+TEST(BuildingEventLog, two) {
+   EXPECT_EQ(1, 1);
+}
+
+
+/* Read Log 1 */
+TEST(ReadLog, one) {
+   EXPECT_EQ(1, 1);
+}
+
+
+/* Read Log 2 */
+TEST(ReadLog, two) {
+   EXPECT_EQ(1, 1);
+}
+
+
+/* Lets delete the earlier log, then create a new event manager
+   the latest_log_id should still be 2 */
+TEST(DeleteEarlierLog, one) {
+   EXPECT_EQ(1, 1);
+}
+
+
+/* Travese log list stuff */
+TEST(TraverseLogList, one) {
+   EXPECT_EQ(1, 1);
+}
+
+
+/* Testing the max limits for event logs */
+TEST(TestMaxLimitLogs, one) {
+   EXPECT_EQ(1, 1);
+}
+
+
+/* Testing the max limits for event logs */
+TEST(TestMaxLimitLogs, two) {
+   EXPECT_EQ(1, 1);
+}
+
+
+/* Create an abundence of logs, then restart with a limited set  */
+/* You should not be able to create new logs until the log size  */
+/* dips below the request number                                 */
+TEST(CreateLogsRestartSet, one) {
+   EXPECT_EQ(1, 1);
+}
+
+
+/* Delete logs to dip below the requested limit */
+TEST(DeleteLogsBelowLimit, one) {
+   EXPECT_EQ(1, 1);
+}
+
+
+/* Create an abundence of logs, then restart with a limited set  */
+/* You should not be able to create new logs until the log size  */
+/* dips below the request number                                 */
+TEST(CreateLogsRestartSet, two) {
+   EXPECT_EQ(1, 1);
+}
+
+
+
 //Run all the tests that were declared with TEST()
 int main(int argc, char **argv)
 {
-   //uint8_t p[] = {0x3, 0x32, 0x34, 0x36};
-   //event_record_t rec, *prec;
-   string s;
+   setup();
+   //event_manager m(eventspath, 0, 0);
+   assert(m.get_managed_size() == 0);
+   //EXPECT_EQ(0, m.get_managed_size());
+   assert(m.next_log() == 0);
+   //EXPECT_EQ(0, m.next_log());
+   m.next_log_refresh();
+   assert(m.next_log() == 0);
+   //EXPECT_EQ(0, m.next_log());
+   assert(m.latest_log_id() == 0);
+   //EXPECT_EQ(0, m.latest_log_id());
+   assert(m.log_count() == 0);
+   //EXPECT_EQ(0, m.log_count());
 
    setup(); //Needed to clear events for the next test
    testing::InitGoogleTest(&argc, argv);
    return RUN_ALL_TESTS();
 }
+
+
+

93f162d19c8f344aff886f9644a2f5dd739f236c
Josh King <jdking@us.ibm.com>
2016-11-14 16:03:36 -0600

    Implemented phosphor-event tests to gtest
    This is the implemented portion of the tests
    over to gtest.  As I have done previously I
    used test.C as a guide to complete the
    implementation of the tests.  One thing worth
    noting is that in the main method I had to
    modify the final few lines of code so that once
    the tests finished running, we would call setup()
    which clears the events created during this
    test.  This is necessary so that if you were
    to run the tests a second time you wouldn't
    continue off from the previous event number
    you were at and instead you will now be starting
    fresh.
    
    Resolved openbmc/openbmc#561
    
    Change-Id: Ic192ac7753c7a9da541e8ed6efb272f43486ef95
    Signed-off-by: Josh King <jdking@us.ibm.com>

--- test/utest.cpp
+++ test/utest.cpp
@@ -1,29 +1,19 @@
-#include "sample.h"
 #include "message.hpp"
-#include "event_messaged.hpp"
 #include <gtest/gtest.h>
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string>
-#include <assert.h>
-#include <unistd.h>
 
 
-string eventspath = "./events";
+constexpr auto eventspath = "./events";
 uint8_t p[] = {0x3, 0x32, 0x34, 0x36};
 event_record_t rec, *prec;
-string s;
-event_manager m(eventspath, 0, 0);
-
+event_manager eventMain(eventspath, 0, 0);
 
 
 void build_event_record(event_record_t *rec,
-                        const char *message,
-                        const char *severity,
-                        const char *association,
-                        const char *reportedby,
-                        const uint8_t *p,
+                        const char* message,
+                        const char* severity,
+                        const char* association,
+                        const char* reportedby,
+                        const uint8_t* p,
                         size_t n)
 {
         rec->message     = (char*) message;
@@ -38,79 +28,173 @@ void build_event_record(event_record_t *rec,
 
 void setup(void)
 {
-        char *cmd = NULL;
+        char *cmd = nullptr;
+        int resultAsp = 0;
+        int resultSys = 0;
 
-        asprintf(&cmd, "exec rm -r %s 2> /dev/null", eventspath.c_str());
-        system(cmd);
+        resultAsp = asprintf(&cmd, "exec rm -r %s 2> /dev/null", eventspath);
+        if(resultAsp == -1){
+            throw std::bad_alloc();
+        }
+        resultSys = system(cmd);
+        if(resultSys != 0){
+            throw std::bad_alloc();
+        }
         free(cmd);
 
-        asprintf(&cmd, "exec mkdir  %s 2> /dev/null", eventspath.c_str());
-        system(cmd);
+        resultAsp = asprintf(&cmd, "exec mkdir  %s 2> /dev/null", eventspath);
+        if(resultAsp == -1){
+            throw std::bad_alloc();
+        }
+        resultSys = system(cmd);
+        if(resultSys != 0){
+            throw std::bad_alloc();
+        }
         free(cmd);
 
         return;
 }
 
-/* First sample Test */
-TEST(FactorialTest, Zero) {
-    EXPECT_EQ(1, Factorial(0));
+
+/* Setting up main */
+TEST(BuildingEventLog, main) {
+   EXPECT_EQ(0, eventMain.get_managed_size());
+   EXPECT_EQ(0, eventMain.next_log());
+   eventMain.next_log_refresh();
+   EXPECT_EQ(0, eventMain.next_log());
+   EXPECT_EQ(0, eventMain.latest_log_id());
+   EXPECT_EQ(0, eventMain.log_count());
 }
 
 
 /* Building 1st Event Log */
 TEST(BuildingEventLog, one) {
-   EXPECT_EQ(1, 1);
    build_event_record(&rec,"Testing Message1", "Info", "Association", "Test", p, 4);
-   EXPECT_EQ(1,  m.create(&rec));
-   EXPECT_EQ(75, m.get_managed_size());
-   EXPECT_EQ(1,  m.log_count());
-   EXPECT_EQ(1,  m.latest_log_id());
-   m.next_log_refresh();
-   EXPECT_EQ(1,  m.next_log());
-   m.next_log_refresh();
+   EXPECT_EQ(1,  eventMain.create(&rec));
+   EXPECT_EQ(75, eventMain.get_managed_size());
+   EXPECT_EQ(1,  eventMain.log_count());
+   EXPECT_EQ(1,  eventMain.latest_log_id());
+   eventMain.next_log_refresh();
+   EXPECT_EQ(1,  eventMain.next_log());
+   eventMain.next_log_refresh();
 }
 
 
 /* Building 2nd Event Log */
 TEST(BuildingEventLog, two) {
-   EXPECT_EQ(1, 1);
+   build_event_record(&rec, "Testing Message2", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(2,   eventMain.create(&rec));
+   EXPECT_EQ(150, eventMain.get_managed_size());
+   EXPECT_EQ(2,   eventMain.log_count());
+   EXPECT_EQ(2,   eventMain.latest_log_id());
+   eventMain.next_log_refresh();
+   EXPECT_EQ(1,   eventMain.next_log());
+   EXPECT_EQ(2,   eventMain.next_log());
 }
 
 
 /* Read Log 1 */
 TEST(ReadLog, one) {
-   EXPECT_EQ(1, 1);
+   string s;
+   EXPECT_EQ(1, eventMain.open(1, &prec));
+   s = prec->message;
+   EXPECT_EQ(0, s.compare("Testing Message1"));
+   eventMain.close(prec);
 }
 
 
 /* Read Log 2 */
 TEST(ReadLog, two) {
-   EXPECT_EQ(1, 1);
+   string s;
+   EXPECT_EQ(2, eventMain.open(2, &prec));
+   s = prec->message;
+   EXPECT_EQ(0, s.compare("Testing Message2"));
+   eventMain.close(prec);
 }
 
 
 /* Lets delete the earlier log, then create a new event manager
    the latest_log_id should still be 2 */
 TEST(DeleteEarlierLog, one) {
-   EXPECT_EQ(1, 1);
+   eventMain.remove(1);
+   EXPECT_EQ(75, eventMain.get_managed_size());
+
+   event_manager eventq(eventspath, 0, 0);
+   EXPECT_EQ(2, eventq.latest_log_id());
+   EXPECT_EQ(1, eventq.log_count());
+   eventMain.next_log_refresh();
 }
 
 
 /* Travese log list stuff */
 TEST(TraverseLogList, one) {
-   EXPECT_EQ(1, 1);
+   try { setup(); }
+   catch(std::bad_alloc&) {
+      std::cerr << "bad_alloc caught: bad allocation of cmd\n";
+      EXPECT_EQ(0, 1); //Fail the test
+   }
+   event_manager eventa(eventspath, 0, 0);
+   EXPECT_EQ(0, eventa.next_log());
+
+   build_event_record(&rec, "Testing list", "Info", "Association", "Test", p, 4);
+   eventa.create(&rec);
+   eventa.create(&rec);
+
+   event_manager eventb(eventspath, 0, 0);
+   EXPECT_EQ(1, eventb.next_log());
 }
 
 
 /* Testing the max limits for event logs */
 TEST(TestMaxLimitLogs, one) {
-   EXPECT_EQ(1, 1);
+   try { setup(); }
+   catch(std::bad_alloc&) {
+      std::cerr << "bad_alloc caught: bad allocation of cmd\n";
+      EXPECT_EQ(0, 1); //Fail the test
+   }
+   //setup();
+   event_manager eventd(eventspath, 75, 0);
+   build_event_record(&rec, "Testing Message1", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(0, eventd.create(&rec));
+
+   event_manager evente(eventspath, 76, 0);
+   build_event_record(&rec, "Testing Message1", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(1, evente.create(&rec));
+
+   try { setup(); }
+   catch(std::bad_alloc&) { std::cerr << "bad_alloc caught: bad allocation of cmd\n";}
+   event_manager eventf(eventspath, 149, 0);
+   build_event_record(&rec, "Testing Message1", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(1, eventf.create(&rec));
+   EXPECT_EQ(0, eventf.create(&rec));
 }
 
 
 /* Testing the max limits for event logs */
 TEST(TestMaxLimitLogs, two) {
-   EXPECT_EQ(1, 1);
+   try { setup(); }
+   catch(std::bad_alloc&) {
+      std::cerr << "bad_alloc caught: bad allocation of cmd\n";
+      EXPECT_EQ(0, 1); //Fail the test
+   }
+   event_manager eventg(eventspath, 300, 1);
+   build_event_record(&rec, "Testing Message1", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(1, eventg.create(&rec));
+   EXPECT_EQ(0, eventg.create(&rec));
+   EXPECT_EQ(1, eventg.log_count());
+
+   try { setup(); }
+   catch(std::bad_alloc&) {
+      std::cerr << "bad_alloc caught: bad allocation of cmd\n";
+      EXPECT_EQ(0, 1); //Fail the test
+   }
+   event_manager eventh(eventspath, 600, 3);
+   build_event_record(&rec, "Testing Message1", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(1, eventh.create(&rec));
+   EXPECT_EQ(2, eventh.create(&rec));
+   EXPECT_EQ(3, eventh.create(&rec));
+   EXPECT_EQ(0, eventh.create(&rec));
+   EXPECT_EQ(3, eventh.log_count());
 }
 
 
@@ -118,13 +202,33 @@ TEST(TestMaxLimitLogs, two) {
 /* You should not be able to create new logs until the log size  */
 /* dips below the request number                                 */
 TEST(CreateLogsRestartSet, one) {
-   EXPECT_EQ(1, 1);
-}
+   try { setup(); }
+   catch(std::bad_alloc&) {
+      std::cerr << "bad_alloc caught: bad allocation of cmd\n";
+      EXPECT_EQ(0, 1); //Fail the test
+   }
+   event_manager eventi(eventspath, 600, 3);
+   build_event_record(&rec, "Testing Message1", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(1, eventi.create(&rec));
+   EXPECT_EQ(2, eventi.create(&rec));
+   EXPECT_EQ(3, eventi.create(&rec));
+   EXPECT_EQ(0, eventi.create(&rec));
+   EXPECT_EQ(3, eventi.log_count());
+   event_manager eventj(eventspath, 600, 1);
+   EXPECT_EQ(3, eventj.log_count());
+   EXPECT_EQ(0, eventj.create(&rec));
+   EXPECT_EQ(3, eventj.log_count());
 
-
-/* Delete logs to dip below the requested limit */
-TEST(DeleteLogsBelowLimit, one) {
-   EXPECT_EQ(1, 1);
+   /* Delete logs to dip below the requested limit */
+   EXPECT_EQ(0, eventj.remove(3));
+   EXPECT_EQ(2, eventj.log_count());
+   EXPECT_EQ(0, eventj.create(&rec));
+   EXPECT_EQ(0, eventj.remove(2));
+   EXPECT_EQ(1, eventj.log_count());
+   EXPECT_EQ(0, eventj.create(&rec));
+   EXPECT_EQ(0, eventj.remove(1));
+   EXPECT_EQ(0, eventj.log_count());
+   EXPECT_EQ(7, eventj.create(&rec));
 }
 
 
@@ -132,7 +236,20 @@ TEST(DeleteLogsBelowLimit, one) {
 /* You should not be able to create new logs until the log size  */
 /* dips below the request number                                 */
 TEST(CreateLogsRestartSet, two) {
-   EXPECT_EQ(1, 1);
+   try { setup(); }
+   catch(std::bad_alloc&) {
+      std::cerr << "bad_alloc caught: bad allocation of cmd\n";
+      EXPECT_EQ(0, 1); //Fail the test
+   }
+   event_manager eventk(eventspath, 600, 100);
+   build_event_record(&rec, "Testing Message1", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(1, eventk.create(&rec));
+   EXPECT_EQ(2, eventk.create(&rec));
+   /* Now we have consumed 150 bytes */
+   event_manager eventl(eventspath, 151, 100);
+   EXPECT_EQ(0, eventl.create(&rec));
+   EXPECT_EQ(0, eventl.remove(2));
+   EXPECT_EQ(4, eventl.create(&rec));
 }
 
 
@@ -140,24 +257,18 @@ TEST(CreateLogsRestartSet, two) {
 //Run all the tests that were declared with TEST()
 int main(int argc, char **argv)
 {
-   setup();
-   //event_manager m(eventspath, 0, 0);
-   assert(m.get_managed_size() == 0);
-   //EXPECT_EQ(0, m.get_managed_size());
-   assert(m.next_log() == 0);
-   //EXPECT_EQ(0, m.next_log());
-   m.next_log_refresh();
-   assert(m.next_log() == 0);
-   //EXPECT_EQ(0, m.next_log());
-   assert(m.latest_log_id() == 0);
-   //EXPECT_EQ(0, m.latest_log_id());
-   assert(m.log_count() == 0);
-   //EXPECT_EQ(0, m.log_count());
-
-   setup(); //Needed to clear events for the next test
+   try { setup(); }
+   catch(std::bad_alloc&) {
+      std::cerr << "bad_alloc caught: bad allocation of cmd\n";
+      EXPECT_EQ(0, 1); //Fail the test
+   }
    testing::InitGoogleTest(&argc, argv);
-   return RUN_ALL_TESTS();
+   int capture = RUN_ALL_TESTS();
+   //Needed to clear events for the next test
+   try { setup(); }
+   catch(std::bad_alloc&) {
+      std::cerr << "bad_alloc caught: bad allocation of cmd\n";
+      EXPECT_EQ(0, 1); //Fail the test
+   }
+   return capture;
 }
-
-
-

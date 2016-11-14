2236e2ad172de8b7cdc1620f39f764d3da24543c
Josh King <jdking@us.ibm.com>
2016-11-14 16:03:36 -0600

    Implemented phosphor-event tests to gtest
    This is the implemented portion of the tests
    over to gtest.  As I have done previously I
    used test.C as a guide to complete the
    implementation of the tests.  One thing worth
    noting is that in the main method I had to
    modify the final few lines of code so that once
    the tests finished running, we would call setup()
    which clears the events created during this
    test.  This is necessary so that if you were
    to run the tests a second time you wouldn't
    continue off from the previous event number
    you were at and instead you will now be starting
    fresh.
    
    Resolved openbmc/openbmc#561
    
    Change-Id: Ic192ac7753c7a9da541e8ed6efb272f43486ef95
    Signed-off-by: Josh King <jdking@us.ibm.com>

--- test/utest.cpp
+++ test/utest.cpp
@@ -1,29 +1,20 @@
-#include "sample.h"
 #include "message.hpp"
-#include "event_messaged.hpp"
 #include <gtest/gtest.h>
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string>
-#include <assert.h>
-#include <unistd.h>
 
 
-string eventspath = "./events";
+std::string eventspath = "./events";
 uint8_t p[] = {0x3, 0x32, 0x34, 0x36};
 event_record_t rec, *prec;
 string s;
 event_manager m(eventspath, 0, 0);
 
 
-
 void build_event_record(event_record_t *rec,
-                        const char *message,
-                        const char *severity,
-                        const char *association,
-                        const char *reportedby,
-                        const uint8_t *p,
+                        const char* message,
+                        const char* severity,
+                        const char* association,
+                        const char* reportedby,
+                        const uint8_t* p,
                         size_t n)
 {
         rec->message     = (char*) message;
@@ -38,28 +29,43 @@ void build_event_record(event_record_t *rec,
 
 void setup(void)
 {
-        char *cmd = NULL;
+        char *cmd = nullptr;
+        int resultAsp = 0;
+        int resultSys = 0;
 
-        asprintf(&cmd, "exec rm -r %s 2> /dev/null", eventspath.c_str());
-        system(cmd);
+        resultAsp = asprintf(&cmd, "exec rm -r %s 2> /dev/null", eventspath.c_str());
+        resultSys = system(cmd);
+        if(resultAsp == -1){
+            printf("The system return value is %d\nMemory allocation wasn't possible for cmd.\n", resultSys);
+            throw std::bad_alloc();
+        }
         free(cmd);
 
-        asprintf(&cmd, "exec mkdir  %s 2> /dev/null", eventspath.c_str());
-        system(cmd);
+        resultAsp = asprintf(&cmd, "exec mkdir  %s 2> /dev/null", eventspath.c_str());
+        resultSys = system(cmd);
+        if(resultAsp == -1){
+            printf("The system return value is %d\nMemory allocation wasn't possible for cmd.\n", resultSys);
+            throw std::bad_alloc();
+        }
         free(cmd);
 
         return;
 }
 
-/* First sample Test */
-TEST(FactorialTest, Zero) {
-    EXPECT_EQ(1, Factorial(0));
+
+/* Setting up main */
+TEST(BuildingEventLog, main) {
+   EXPECT_EQ(0, m.get_managed_size());
+   EXPECT_EQ(0, m.next_log());
+   m.next_log_refresh();
+   EXPECT_EQ(0, m.next_log());
+   EXPECT_EQ(0, m.latest_log_id());
+   EXPECT_EQ(0, m.log_count());
 }
 
 
 /* Building 1st Event Log */
 TEST(BuildingEventLog, one) {
-   EXPECT_EQ(1, 1);
    build_event_record(&rec,"Testing Message1", "Info", "Association", "Test", p, 4);
    EXPECT_EQ(1,  m.create(&rec));
    EXPECT_EQ(75, m.get_managed_size());
@@ -73,44 +79,99 @@ TEST(BuildingEventLog, one) {
 
 /* Building 2nd Event Log */
 TEST(BuildingEventLog, two) {
-   EXPECT_EQ(1, 1);
+   build_event_record(&rec, "Testing Message2", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(2,   m.create(&rec));
+   EXPECT_EQ(150, m.get_managed_size());
+   EXPECT_EQ(2,   m.log_count());
+   EXPECT_EQ(2,   m.latest_log_id());
+   m.next_log_refresh();
+   EXPECT_EQ(1,   m.next_log());
+   EXPECT_EQ(2,   m.next_log());
 }
 
 
 /* Read Log 1 */
 TEST(ReadLog, one) {
-   EXPECT_EQ(1, 1);
+   EXPECT_EQ(1, m.open(1, &prec));
+   s = prec->message;
+   EXPECT_EQ(0, s.compare("Testing Message1"));
+   m.close(prec);
 }
 
 
 /* Read Log 2 */
 TEST(ReadLog, two) {
-   EXPECT_EQ(1, 1);
+   EXPECT_EQ(2, m.open(2, &prec));
+   s = prec->message;
+   EXPECT_EQ(0, s.compare("Testing Message2"));
+   m.close(prec);
 }
 
 
 /* Lets delete the earlier log, then create a new event manager
    the latest_log_id should still be 2 */
 TEST(DeleteEarlierLog, one) {
-   EXPECT_EQ(1, 1);
+   m.remove(1);
+   EXPECT_EQ(75, m.get_managed_size());
+
+   event_manager q(eventspath, 0, 0);
+   EXPECT_EQ(2, q.latest_log_id());
+   EXPECT_EQ(1, q.log_count());
+   m.next_log_refresh();
 }
 
 
 /* Travese log list stuff */
 TEST(TraverseLogList, one) {
-   EXPECT_EQ(1, 1);
+   system("exec rm -r ./events/* 2> /dev/null");
+   event_manager a(eventspath, 0, 0);
+   EXPECT_EQ(0, a.next_log());
+
+   build_event_record(&rec, "Testing list", "Info", "Association", "Test", p, 4);
+   a.create(&rec);
+   a.create(&rec);
+
+   event_manager b(eventspath, 0, 0);
+   EXPECT_EQ(1, b.next_log());
 }
 
 
 /* Testing the max limits for event logs */
 TEST(TestMaxLimitLogs, one) {
-   EXPECT_EQ(1, 1);
+   setup();
+   event_manager d(eventspath, 75, 0);
+   build_event_record(&rec, "Testing Message1", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(0, d.create(&rec));
+
+   event_manager e(eventspath, 76, 0);
+   build_event_record(&rec, "Testing Message1", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(1, e.create(&rec));
+
+   setup();
+   event_manager f(eventspath, 149, 0);
+   build_event_record(&rec, "Testing Message1", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(1, f.create(&rec));
+   EXPECT_EQ(0, f.create(&rec));
 }
 
 
 /* Testing the max limits for event logs */
 TEST(TestMaxLimitLogs, two) {
-   EXPECT_EQ(1, 1);
+   setup();
+   event_manager g(eventspath, 300, 1);
+   build_event_record(&rec, "Testing Message1", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(1, g.create(&rec));
+   EXPECT_EQ(0, g.create(&rec));
+   EXPECT_EQ(1, g.log_count());
+
+   setup();
+   event_manager h(eventspath, 600, 3);
+   build_event_record(&rec, "Testing Message1", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(1, h.create(&rec));
+   EXPECT_EQ(2, h.create(&rec));
+   EXPECT_EQ(3, h.create(&rec));
+   EXPECT_EQ(0, h.create(&rec));
+   EXPECT_EQ(3, h.log_count());
 }
 
 
@@ -118,13 +179,29 @@ TEST(TestMaxLimitLogs, two) {
 /* You should not be able to create new logs until the log size  */
 /* dips below the request number                                 */
 TEST(CreateLogsRestartSet, one) {
-   EXPECT_EQ(1, 1);
-}
+   setup();
+   event_manager i(eventspath, 600, 3);
+   build_event_record(&rec, "Testing Message1", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(1, i.create(&rec));
+   EXPECT_EQ(2, i.create(&rec));
+   EXPECT_EQ(3, i.create(&rec));
+   EXPECT_EQ(0, i.create(&rec));
+   EXPECT_EQ(3, i.log_count());
+   event_manager j(eventspath, 600, 1);
+   EXPECT_EQ(3, j.log_count());
+   EXPECT_EQ(0, j.create(&rec));
+   EXPECT_EQ(3, j.log_count());
 
-
-/* Delete logs to dip below the requested limit */
-TEST(DeleteLogsBelowLimit, one) {
-   EXPECT_EQ(1, 1);
+   /* Delete logs to dip below the requested limit */
+   EXPECT_EQ(0, j.remove(3));
+   EXPECT_EQ(2, j.log_count());
+   EXPECT_EQ(0, j.create(&rec));
+   EXPECT_EQ(0, j.remove(2));
+   EXPECT_EQ(1, j.log_count());
+   EXPECT_EQ(0, j.create(&rec));
+   EXPECT_EQ(0, j.remove(1));
+   EXPECT_EQ(0, j.log_count());
+   EXPECT_EQ(7, j.create(&rec));
 }
 
 
@@ -132,7 +209,16 @@ TEST(DeleteLogsBelowLimit, one) {
 /* You should not be able to create new logs until the log size  */
 /* dips below the request number                                 */
 TEST(CreateLogsRestartSet, two) {
-   EXPECT_EQ(1, 1);
+   setup();
+   event_manager k(eventspath, 600, 100);
+   build_event_record(&rec, "Testing Message1", "Info", "Association", "Test", p, 4);
+   EXPECT_EQ(1, k.create(&rec));
+   EXPECT_EQ(2, k.create(&rec));
+   /* Now we have consumed 150 bytes */
+   event_manager l(eventspath, 151, 100);
+   EXPECT_EQ(0, l.create(&rec));
+   EXPECT_EQ(0, l.remove(2));
+   EXPECT_EQ(4, l.create(&rec));
 }
 
 
@@ -141,23 +227,8 @@ TEST(CreateLogsRestartSet, two) {
 int main(int argc, char **argv)
 {
    setup();
-   //event_manager m(eventspath, 0, 0);
-   assert(m.get_managed_size() == 0);
-   //EXPECT_EQ(0, m.get_managed_size());
-   assert(m.next_log() == 0);
-   //EXPECT_EQ(0, m.next_log());
-   m.next_log_refresh();
-   assert(m.next_log() == 0);
-   //EXPECT_EQ(0, m.next_log());
-   assert(m.latest_log_id() == 0);
-   //EXPECT_EQ(0, m.latest_log_id());
-   assert(m.log_count() == 0);
-   //EXPECT_EQ(0, m.log_count());
-
-   setup(); //Needed to clear events for the next test
    testing::InitGoogleTest(&argc, argv);
-   return RUN_ALL_TESTS();
+   int capture = RUN_ALL_TESTS();
+   setup(); //Needed to clear events for the next test
+   return capture;
 }
-
-
-
